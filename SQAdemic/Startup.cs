257d8231f5395ca3f69b636c29b5f959ff12f9ea
budocf <budocf@.rose-hulman.edu>
2015-03-24 11:48:46 -0400

    boilerplate code first commit

---
+++ SQAdemic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SQAdemic.Startup))]
+namespace SQAdemic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
