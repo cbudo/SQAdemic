e5c089e4c4fd1ed3e71b41108dd42be277259965
budocf <budocf@.rose-hulman.edu>
2015-03-26 14:30:47 -0400

    added players

--- SQAdemic/Models/PlayerModels.cs
+++ SQAdemic/Models/PlayerModels.cs
@@ -2,14 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SQAdemic.Models.GameBoardModels;
 
 namespace SQAdemic.Models
 {
     public class PlayerModels
     {
-        public class PlayerBase
+        public class Player
         {
-            public List<Card> Hand
+            public List<GameBoardModels.Card> hand = new List<GameBoardModels.Card>();
+            public GameBoardModels.City location { get; set; }
+            public Player()
+            {
+                    
+            }
+            public void move(GameBoardModels.City destination)
+            {
+
+            }
         }
+        public class Medic : Player
+        {
+            public Medic()
+            {
+
+            }
+        }
+        public class OpExpert : Player
+        {
+            public OpExpert()
+            {
+
+            }
+        }
+        public class Researcher : Player
+        {
+            public Researcher()
+            {
+
+            }
+        }
+        public class Scientist : Player
+        {
+            public Scientist()
+            {
+
+            }
+        }
+        public class Dispatcher :Player
+        {
+            public Dispatcher()
+            {
+
+            }
+        }
+        
     }
 }
