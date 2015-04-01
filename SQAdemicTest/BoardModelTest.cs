b6cccbe33abcf2f38b1e64c7d0981510d1832019
budocf <budocf@.rose-hulman.edu>
2015-04-01 16:36:22 -0400

    added test

---
+++ SQAdemicTest/BoardModelTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SQAdemic;
+
+namespace SQAdemicTest
+{
+    [TestFixture()]
+    class BoardModelTest
+    {
+        private SQAdemic.Models.GameBoardModels BoardModel;
+        [Test()]
+        public void TestThatBoardInitializes()
+        {
+            BoardModel = new SQAdemic.Models.GameBoardModels();
+            int[] cubeExpected = {24,24,24,24};
+            Assert.AreEqual(cubeExpected, BoardModel.getCubes());
+            
+        }
+
+    }
+    
+}
