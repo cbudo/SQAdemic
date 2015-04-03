da6aa7d0eab631b86f27661420b30cb8831886e8
budocf <budocf@.rose-hulman.edu>
2015-04-02 21:12:43 -0400

    updated to fix other changes

--- SQAdemicTest/BoardModelTest.cs
+++ SQAdemicTest/BoardModelTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using SQAdemic;
+using Rhino.Mocks;
 
 namespace SQAdemicTest
 {
@@ -17,7 +18,10 @@ namespace SQAdemicTest
         {
             BoardModel = new SQAdemic.Models.GameBoardModels();
             int[] cubeExpected = {24,24,24,24};
+            SQAdemic.Models.GameBoardModels.CURESTATE notCured = SQAdemic.Models.GameBoardModels.CURESTATE.NotCured;
+            SQAdemic.Models.GameBoardModels.CURESTATE[] cures = { notCured, notCured, notCured, notCured };
             Assert.AreEqual(cubeExpected, BoardModel.getCubes());
+            Assert.AreEqual(cures, BoardModel.getCures());
             
         }
 
